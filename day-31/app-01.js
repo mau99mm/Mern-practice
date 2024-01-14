b7c5f053a88582cfe6b3d96756ba801c12773491
mau99mm <mauriciomadera43@gmail.com>
2024-01-14 11:42:09 -0600

    Javascript Variables

---
+++ day-31/app-01.js
@@ -0,0 +1,13 @@
+var num1 = 10
+num1 = 20 //Reassigned
+console.log('Value of Num1: ')
+console.log(num1)
+
+var num2 = 20
+num2 = 30 //Reassigned
+console.log('Value of Num2: ')
+console.log(num2)
+
+var result = num1 + num2
+console.log('Addition is: ')
+console.log(result)
