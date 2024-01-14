40a263b1a58a962da5c3494d21c9527fbd42aadb
mau99mm <mauriciomadera43@gmail.com>
2024-01-14 14:10:37 -0600

    javascript: variables and data types

---
+++ day-31/app-02.js
@@ -0,0 +1,20 @@
+console.log('Numeric Type')
+var num = 12 // numeric 
+console.log(num)
+
+console.log('Decimal Numeric Type')
+var sal = 23.45 //numeric
+console.log(sal)
+
+console.log('Boolean Type')
+var isOkay = false // boolean
+console.log(isOkay)
+
+console.log('String Type')
+var nm = 'Atomic Habits' //String
+console.log(nm)
+
+console.log('String Type')
+var isbn = "h235899" //String
+console.log(isbn)
+
