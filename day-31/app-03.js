cda6915058ec4a8cfb17837d1e0241f2e910821a
mau99mm <mauriciomadera43@gmail.com>
2024-01-14 14:33:51 -0600

    javascript vnd: object type

---
+++ day-31/app-03.js
@@ -0,0 +1,18 @@
+console.log('Date is')
+var dob = new Date() //object
+console.log(dob)
+
+console.log('Object 1 is')
+var obj1 = { //object
+    nm : 'abc',
+    make: 'mak1',
+    model: 123,
+    isEv: true
+}
+console.log(obj1)
+
+console.log('Object 2 is')
+var obj2 = { a : 1, b : 2, c : 3 }
+console.log(obj2)
+
+
