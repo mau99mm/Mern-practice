27edc3efcfcda7e0f51eff40a5fa587487888110
mau99mm <mauriciomadera43@gmail.com>
2024-01-15 21:23:13 -0600

    Javascript: Naming Conventions

---
+++ day-33/app-03.js
@@ -0,0 +1,15 @@
+//var 10abc = 11 // -> Not Valid
+//var a-b-c = 30 // -> Not Valid
+
+var abc = 11
+var _abc = 20
+var ABC = 90
+var $abc = 90
+var a_b_c = 90
+var a_10_b_20_c_30 = 90
+
+// Standard 
+var employeeName = 'abc' // camel casing
+var empnm = 'pqr'
+
+var salaryAccountDetails = 1234
