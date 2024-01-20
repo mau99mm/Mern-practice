f433723a3349e459da60c1ea00cbe5a3242bc51e
mau99mm <mauriciomadera43@gmail.com>
2024-01-19 20:24:32 -0600

    Javascript: es6 let and conts

---
+++ day-36/app-01.js
@@ -0,0 +1,9 @@
+//in es5
+var a = 10
+
+//after es6
+let b = 20
+b = 30
+const c = 30 //constant value of the constant can never change
+
+// c = 90
---
+++ day-36/app-02.js
@@ -0,0 +1,9 @@
+const a = 20
+
+if(true) {
+    console.log('Entered in if condition')
+    const a = 30
+    console.log(a)
+}
+
+console.log(a)
