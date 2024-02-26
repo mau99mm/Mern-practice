352c8e03b70b1b2bee01e79bfaa91b0fd1d1730a
mau99mm <mauriciomadera43@gmail.com>
2024-02-26 14:03:35 -0600

    Javascript: regex basics

---
+++ day-48/app-01.js
@@ -0,0 +1,8 @@
+// I need how many digits are there, in given string 
+// first check are there any digits in string
+const msg2 = 'abc123'
+const regex3 = /\d/g
+const arr = msg2.match(regex3)
+console.log(arr.length)
+
+
---
+++ day-48/app-02.js
@@ -0,0 +1,9 @@
+const txt5 = 'Start of the line'
+const rgx5 = /^Start/
+console.log(rgx5.test(txt5))
+
+
+
+
+
+
