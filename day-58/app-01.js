acd0ffdfaac1087c78f1fa3c0dfecdbc392ae907
mau99mm <mauriciomadera43@gmail.com>
2024-09-26 12:21:59 -0600

    Javascript : Event Loop and Callbacks

---
+++ day-58/app-01.js
@@ -0,0 +1,22 @@
+console.log(`Start`)
+setTimeout(() => console.log('Middle'), 300)
+console.log(`End1`)
+console.log(`End2`)
+console.log(`End3`)
+console.log(`End4`)
+
+for(let i = 0 ; i < 100; i++) {
+    console.log('ending')
+}
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ day-58/app-02.js
@@ -0,0 +1,11 @@
+console.log(`Connecting to FB`)
+setTimeout( () => console.log('Connected to FB'), 2000)
+
+console.log(`Processing Image`)
+setTimeout( () => console.log('Image Processing Completed'), 3000)
+
+console.log(`Processing Text`)
+setTimeout( () => console.log('Text Processing'), 2500)
+
+
+
