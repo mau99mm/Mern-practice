3b273edd3a39be220a72ec5009ccc597c058d9b4
mau99mm <mauriciomadera43@gmail.com>
2024-11-05 10:53:11 -0600

    Dom event handling

---
+++ day-63/app-01.js
@@ -0,0 +1,50 @@
+function onBdLd(ev, p) {
+    console.log(`Body Loaded`)
+    console.log(ev)
+    p.innerText = `${ev.x}, ${ev.y}`
+}
+
+function onYesClicked() {
+    console.log('Yes Clicked')
+    alert('Yes Clicked')
+}
+
+document.addEventListener('DOMContentLoaded', function(ev) {
+    console.log(`Content Loaded`)
+    console.log(ev)
+
+    const btnNo = document.getElementById('btnNo')
+    btnNo.onclick = btnEv => {
+    alert(`No Clicked`)
+    console.log(btnEv)
+    }
+
+    const dv = document.getElementById('dvCol')
+    dv.onmouseenter = () => dv.style.backgroundColor = 'red'
+    dv.onmouseleave = () => dv.style.backgroundColor = 'cadetblue'
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
