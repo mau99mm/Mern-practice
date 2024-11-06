0d0fddaac7a1101b8cae064638e44e583cb0b03c
mau99mm <mauriciomadera43@gmail.com>
2024-11-06 11:25:26 -0600

    Dom: query selector

---
+++ day-64/app-01.js
@@ -0,0 +1,31 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const el = document.querySelector('body :nth-child(3)')
+    console.log(el.innerText)
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
